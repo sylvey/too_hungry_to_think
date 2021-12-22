d0ab415dc47c51d2cc2dd195f1b0ded7f183e011
Sylvey <a.few.star@gmail.com>
2021-12-22 19:53:09 +0800

    navBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import '../src/css/NavBar.css';
+//import { makeStyles } from '@mui/styles';
+// import Button from '@mui/material/Button';
 
+const loginRegisterButton = {backgroundColor: "#FA1414" , marginRight: "10px", color: "white"};
+const NavBarBackground = {background: 'linear-gradient(90deg, white, #FAAC8A 100%)'}
 function App() {
+  // const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" style = {NavBarBackground}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <img src = "logo192.png" width="50px" height="50px"></img>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1}} style = {{color:'#000000'}}>
+            {"To Hungry \n To Think"}
+          </Typography>
+          <Button style = {loginRegisterButton}>Register</Button>
+          <Button style = {loginRegisterButton}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+
   );
 }
 
