73bef3c02ce0dac9d566c006365abba8c910d455
Sylvey <a.few.star@gmail.com>
2021-12-22 20:00:42 +0800

    NavBarComponents

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import '../src/css/NavBar.css';
+import NavBar from './components/NavBar';
+import { Box } from '@mui/material';
 //import { makeStyles } from '@mui/styles';
 // import Button from '@mui/material/Button';
 
@@ -17,24 +12,7 @@ function App() {
   // const classes = useStyles();
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style = {NavBarBackground}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <img src = "logo192.png" width="50px" height="50px"></img>
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1}} style = {{color:'#000000'}}>
-            {"To Hungry \n To Think"}
-          </Typography>
-          <Button style = {loginRegisterButton}>Register</Button>
-          <Button style = {loginRegisterButton}>Login</Button>
-        </Toolbar>
-      </AppBar>
+      <NavBar />
     </Box>
 
   );
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+
+const loginRegisterButton = {backgroundColor: "#FA1414" , marginRight: "10px", color: "white"};
+const NavBarBackground = {background: 'linear-gradient(90deg, white, #FAAC8A 100%)'}
+function NavBar() {
+  return (
+      <AppBar position="static" style = {NavBarBackground}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <img src = "logo192.png" width="50px" height="50px"></img>
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1}} style = {{color:'#000000'}}>
+            {"To Hungry \n To Think"}
+          </Typography>
+          <Button style = {loginRegisterButton}>Register</Button>
+          <Button style = {loginRegisterButton}>Login</Button>
+        </Toolbar>
+      </AppBar>
+
+
+  );
+}
+
+export default NavBar;
