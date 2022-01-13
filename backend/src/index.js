f243706e7f40be39764443ed72b79fa4240c69c7
lineinna <waskevin1125@gmail.com>
2022-01-13 23:37:07 +0800

    LoginBackend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,31 @@ import mongo from "./mongo.js";
 import server from "./server.js";
 
 import "dotenv-defaults/config.js";
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+const users = [];
+function upsert(array, item) {
+  const i = array.findIndex((_item) => _item.email === item.email);
+  if (i > -1) array[i] = item;
+  else array.push(item);
+}
 
 mongo.connect();
 const port = process.env.PORT | 5000;
+
+server.post('/api/google-login', async (req, res) => {
+  const { token } = req.body;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID,
+  });
+  const { name, email, picture } = ticket.getPayload();
+  upsert(users, { name, email, picture });
+  res.status(201);
+  res.json({ name, email, picture });
+});
+
+
 
 server.start({ port }, () => {
   console.log(`The server is up on port ${port}!`);
