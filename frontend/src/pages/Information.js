fe7648f0b6d7929363252ff2775f41431452be1a
samlah <b08505005@ntu.edu.tw>
2022-01-15 06:07:38 +0800

    information set up more

--- frontend/src/pages/Information.js
+++ frontend/src/pages/Information.js
@@ -9,22 +9,83 @@ import { useState, useEffect } from 'react';
 import Page from "../Containers/Page";
 import RightFrame from "../Containers/RightFrame";
 import { Background, CenterDiv, MainDiv, RightDiv } from "../Containers/BackGround";
+import food from '../hardData/food.png'
 
+const BackgroundInf = styled.div`
+    display: flex;
+    flex-direction: row;
+    // width: 100%;
+    height: 65em;
 
+    background-size: cover;
+    background-position: fixed;
+
+    justify-content: center;
+    // background-color: white;
+    background-image:url(${food});
+    // background-size: cover;
+    @media(max-width:1900px){ 
+
+        height: 90em;
+        // background-position: center;
+        // background-position-y: 0;
+        // background-attachment: fixed;
+   }
+    @media(max-width:1300px){ 
+
+        height: 100em;
+        // background-position: center;
+        // background-position-y: 0;
+        // background-attachment: fixed;
+   }
+    
+`
+
+const CenterDivInf = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 70%;
+    max-width: 1200px;
+    min-width: 500px;
+    //height: 800px;
+    //background: red;
+    // justify-content: center;
+`
+
+export const MainDivInf = styled.div`
+    display: flex;
+    flex: 7;
+    max-width: 900px;
+    min-width: 312.5px;
+    //height: 100px;
+    //background: yellow;
+    // align-items: center;
+    // justify-content: center;
+    margin-top: 180px;
+`
+
+export const RightDivInf = styled.div`
+    flex: 4;
+    //display: flex;
+    //background-color: red;
+    max-width: 500px;
+    min-width: 187.5px;
+    margin-top: 10px;
+`
 
 export default function Information(props) {
     console.log(props.location.state.id);
     return(
-        <Background>
-            <CenterDiv>
-                <MainDiv>
+        <BackgroundInf>
+            <CenterDivInf>
+                <MainDivInf>
                     <Page
                     props={props}/>
-                </MainDiv>
-                <RightDiv>
+                </MainDivInf>
+                <RightDivInf>
                     <RightFrame/>
-                </RightDiv> 
-            </CenterDiv>         
-        </Background>
+                </RightDivInf> 
+            </CenterDivInf>         
+        </BackgroundInf>
     )
 }
