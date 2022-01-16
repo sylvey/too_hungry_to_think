8262034d697bf7f74d608acd4231608d990ed97b
lineinna <waskevin1125@gmail.com>
2022-01-16 10:51:13 +0800

    CORSsetting

--- backend/src/publish.js
+++ backend/src/publish.js
@@ -3,6 +3,11 @@ const express = require('express');
 export default (app) => {
     app.use(express.static(path.join(__dirname, '../../frontend/build')));
     app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
-);
+        res.sendFile(path.join(__dirname, '../../frontend/build/index.html'))
+    );
+    app.use(
+        cors({
+            origin: "http://localhost:3000", 
+            credentials: true,
+    }));
 }
